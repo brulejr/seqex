65fcd4bd34b79845f3d9b5dabca9b18a6587f67e
Jon Brule <brulejr@gmail.com>
2013-10-20 07:42:48 -0400

    Added JSON schema to Device using spumko/joi

---
+++ test/api/program/DeviceSpec.js
@@ -0,0 +1,56 @@
+/*
+ * seqex: test/api/device/DeviceExecutorSpec.js
+ * https://github.com/jbrule/seqex
+ *
+ * Copyright (c) 2013 Jon Brule
+ * Licensed under the MIT license.
+ */
+(function() {
+  'use strict';
+
+  // module dependencies
+  require('../../../utils');
+  var chai = require('chai');
+  var expect = chai.expect;
+  var Device = requireLib('/api/program/Device');
+
+  // test device data
+  var device_good = {
+    id: "DEVICE_1",
+    description: "This is device #1",
+    enabled: true
+  };
+
+  var device_bad = {
+    id: "DEVICE_@#@",
+    description: "This is device #1"
+  };
+
+  describe('Device', function () {
+
+    it('should be defined', function() {
+      expect(Device).to.not.be.undefined;
+    });
+
+    describe('constructor', function () {
+
+      it('should build a new instance for good data', function() {
+        var device = new Device(device_good);
+        expect(device).to.not.be.undefined;
+        expect(device).to.have.property('id', 'DEVICE_1');
+        expect(device).to.have.property('enabled', true);
+        expect(device).to.have.property('description', 'This is device #1');
+      });
+
+      it('should throw an error for bad data', function() {
+        var badConstructor = function() {
+          return new Device(device_bad);
+        };
+        expect(badConstructor).to.throw(Error);
+      });
+
+    });
+
+  });
+
+}());  
